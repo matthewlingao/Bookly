cbd96499412b5efbfcb080f03dd3c10b76ebaa79
matthewlingao <mattlingao@gmail.com>
2026-06-08 19:30:02 -0400

    Implement book deletion using @PostMapping and update background styling

--- Bookly/src/main/java/com/enterprise/java/controllers/MainController.java
+++ Bookly/src/main/java/com/enterprise/java/controllers/MainController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -83,5 +84,11 @@ public class MainController {
     @ResponseBody
     public List<Review> getReviewsByBook(@PathVariable int bookid) {
         return reviewService.getReviewsByBookid(bookid);
+    }
+    
+    @PostMapping("/deleteBook/{bookid}")
+    public String deleteBook(@PathVariable int bookid) {
+    bookService.deleteBook(bookid);
+    return "redirect:/";
     }
 }
